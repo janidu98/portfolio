7bc7abf84a0b060f7e46b1c93baef9f04f06269d
janidu98 <janinduchathushan@gmail.com>
2024-03-12 22:05:30 +0530

    add react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 // import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
