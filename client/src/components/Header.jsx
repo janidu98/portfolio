9b6a0b04804b38834c3c1e464c39ff255b289b3b
janidu98 <janinduchathushan@gmail.com>
2024-03-13 15:09:27 +0530

    add header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='p-5 bg-primary flex justify-center'>
+      <h1 className='text-tertiary text-4xl font-semibold'>My Portfolio</h1>
+    </div>
+  )
+}
+
+export default Header
