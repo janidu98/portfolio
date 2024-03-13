3376a965e59cb84df2135ab09de1ae1971a73f7f
janidu98 <janinduchathushan@gmail.com>
2024-03-13 15:42:15 +0530

    complete intro section UI

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Header from '../../components/Header'
+import Intro from './Intro'
+
+const Home = () => {
+  return (
+    <div >
+      <Header />
+      
+      <div className='bg-primary px-40'>
+        <Intro />
+      </div>
+    </div>
+  )
+}
+
+export default Home
