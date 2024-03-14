75747193d9d6372da455539ec1227054c39ad599
janidu98 <janinduchathushan@gmail.com>
2024-03-15 01:50:58 +0530

    change title ui

--- client/src/components/SectionTitle.jsx
+++ client/src/components/SectionTitle.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const SectionTitle = ({ title }) => {
   return (
-    <div className='flex gap-10 items-center py-10'>
-      <h1 className='text-3xl text-white'>{title}</h1>
-      <div className='w-80 h-[1px] bg-tertiary'></div>
+    <div className='flex gap-10 items-center py-10 '>
+      <h1 className='text-3xl text-yellow-300 flex items-center w-1/4'>{title}</h1>
+      <div className='w-full h-[0.5px] bg-tertiary'></div>
     </div>
   )
 }
